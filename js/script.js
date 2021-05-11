2d46cc5133c30554f3ac73faa7ee24574d13fbde
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-05-11 22:12:20 +0200

    refactoring js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
-console.log("Hello my friend how are you");
+{
+    const welcome = () => {
+        console.log("Hello my friend how are you");
+    }
+    
+    const toggleBackgroundImage = () => {
+        const dayPhoto = document.querySelector(".background");
+        dayPhoto.classList.toggle("background-image");
+        changeBackgroundButton.innerText = dayPhoto.classList.contains("background-image") ? "Noc" : "Dzień"
+    };
+    
+    const toggleHelmetPhoto = () => {
+        const helmetPhoto = document.querySelector(".background__image");
+        helmetPhoto.classList.toggle("main--remove");
+        removeHelmetPhotoButton.innerText = helmetPhoto.classList.contains("main--remove") ? "Pokaż" : "Ukryj"
+    };
+    
+    const init = () =>{
+        const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+        const removeHelmetPhotoButton = document.querySelector(".js-removeImageButton");
+        
+        changeBackgroundButton.addEventListener("click", toggleBackgroundImage);
+        
+        removeHelmetPhotoButton.addEventListener("click", toggleHelmetPhoto);
 
-let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
-let dayPhoto = document.querySelector(".background");
-let removeHelmetPhotoButton = document.querySelector(".js-removeImageButton");
-let helmetPhoto = document.querySelector(".background__image");
-
-
-changeBackgroundButton.addEventListener("click", () => {
-    dayPhoto.classList.toggle("background-image");
-
-    changeBackgroundButton.innerText = dayPhoto.classList.contains("background-image") ? "Noc" : "Dzień"
-});
-
-removeHelmetPhotoButton.addEventListener("click", () => {
-    helmetPhoto.classList.toggle("main--remove");
-
-    removeHelmetPhotoButton.innerText = helmetPhoto.classList.contains("main--remove") ? "Pokaż" : "Ukryj"
-});
-
-
-
+        welcome();
+    };
+    
+    init();
+    
+}
