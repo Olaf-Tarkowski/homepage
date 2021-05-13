14727ae20faad4bc6da8222d4c0cc0e80aea5ea6
Olaf Tarkowski <olaftarkowski97@gmail.com>
2021-05-13 20:55:30 +0200

    fixing button change

--- js/script.js
+++ js/script.js
@@ -2,30 +2,32 @@
     const welcome = () => {
         console.log("Hello my friend how are you");
     }
-    
-    const toggleBackgroundImage = () => {
+
+    const onChangeBackgroundButton = () => {
         const dayPhoto = document.querySelector(".background");
+        const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
         dayPhoto.classList.toggle("background-image");
         changeBackgroundButton.innerText = dayPhoto.classList.contains("background-image") ? "Noc" : "Dzień"
     };
-    
-    const toggleHelmetPhoto = () => {
+
+    const onRemoveHelmetPhotoButton = () => {
         const helmetPhoto = document.querySelector(".background__image");
+        const removeHelmetPhotoButton = document.querySelector(".js-removeImageButton");
         helmetPhoto.classList.toggle("main--remove");
         removeHelmetPhotoButton.innerText = helmetPhoto.classList.contains("main--remove") ? "Pokaż" : "Ukryj"
     };
-    
-    const init = () =>{
+
+    const init = () => {
         const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
         const removeHelmetPhotoButton = document.querySelector(".js-removeImageButton");
-        
-        changeBackgroundButton.addEventListener("click", toggleBackgroundImage);
-        
-        removeHelmetPhotoButton.addEventListener("click", toggleHelmetPhoto);
+
+        changeBackgroundButton.addEventListener("click", onChangeBackgroundButton);
+
+        removeHelmetPhotoButton.addEventListener("click", onRemoveHelmetPhotoButton);
 
         welcome();
     };
-    
+
     init();
-    
+
 }
